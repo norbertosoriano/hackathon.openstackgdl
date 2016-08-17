4f37b34c3dbc11fa556a320e6b68fdc8d8ccbadd
Norberto J Soriano Hernandez <norberto.j.soriano.hernandez@intel.com>
2016-08-17 17:01:59 -0500

    Adjustments to init.sh
    - Install git.
    - Replace wget with git clone.

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert git -y
 
-#wget https://raw.githubusercontent.com/openstack-hackathon/training-shade-sdk/master/getting_started/challenge/shade-demo.html
-#sudo cp shade-demo.html /var/www/html/index.html
+#git clone https://github.com/norbertosoriano/hackathon.openstackgdl.git
+#sudo cp -r hackathon.openstackgdl/pokedex/* /var/www/html
 
-wget https://github.com/norbertosoriano/hackathon.openstackgdl/blob/master/pokedex 
-sudo cp -r pokedex/* /var/www/html/
+wget https://raw.githubusercontent.com/openstack-hackathon/training-shade-sdk/master/getting_started/challenge/shade-demo.html
+sudo cp shade-demo.html /var/www/html/index.html
 
